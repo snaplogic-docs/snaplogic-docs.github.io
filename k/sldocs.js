ab02737f5122113d7b5a800a1057ee16f462e0c5
eltesoro-snap <etesoro@snaplogic.com>
2022-04-23 00:04:41 -0700

    APIM-160 Revamp APIM topics Added a JScript solution to convert outputclass="details" to <details>.

--- k/sldocs.js
+++ k/sldocs.js
@@ -4,22 +4,27 @@
 
 
 $(document).ready(function() {
-  $(".prereq").prepend('<h2 class="title sectiontitle">Prerequisites</h2>');
-  $(".result").prepend('<h2 class="title sectiontitle">Results</h2>');
-  $(".example").prepend('<h2 class="title sectiontitle">Example</h2>');
-  $(".tasktroubleshooting").prepend('<h2 class="title sectiontitle">Troubleshooting</h2>');
-  $(".postreq").prepend('<h2 class="title sectiontitle">Next steps</h2>');
-  $(".related-links").prepend('<h2 class="title sectiontitle">Related links</h2>');
+  $('.prereq').prepend('<h2 class="title sectiontitle">Prerequisites</h2>');
+  $('.result').prepend('<h2 class="title sectiontitle">Results</h2>');
+  $('.example').prepend('<h2 class="title sectiontitle">Example</h2>');
+  $('.tasktroubleshooting').prepend('<h2 class="title sectiontitle">Troubleshooting</h2>');
+  $('.postreq').prepend('<h2 class="title sectiontitle">Next steps</h2>');
+  $('.related-links').prepend('<h2 class="title sectiontitle">Related links</h2>');
 
   /* The 'steps' class is in the ol tag, not in the section tag. */
-  $("section:has(ol.steps)").prepend('<h2 class="title sectiontitle">Steps</h2>');
-  $("section:has(ul.steps-unordered)").prepend('<h2 class="title sectiontitle">Steps</h2>');
+  $('section:has(ol.steps)').prepend('<h2 class="title sectiontitle">Steps</h2>');
+  $('section:has(ul.steps-unordered)').prepend('<h2 class="title sectiontitle">Steps</h2>');
 
   /* The following is a hack to fix the extra leading blank char with normalize-space. */
-  $(".normalize-space code").each( function() {
+  $('.normalize-space code').each( function() {
     $(this).text( $(this).text().slice(1) );
   });
 
+
+  /* Details */
+  /* $('p').replaceWith($('<div/>').html($('p').html())); */
+  $('.details').replaceWith( $('<details/>').html($('.details').html()));
+  $('.detsumm').replaceWith( $('<summary/>').html($('.detsumm').html()));
 
   /* Tabs */
   $('.tablink').click( function() {
